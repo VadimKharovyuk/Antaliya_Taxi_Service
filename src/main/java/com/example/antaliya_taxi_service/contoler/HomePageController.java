001923fbab2b696b4ed1863c0366992cdeb53573
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-05-18 02:04:21 +0300

    add HomePageController

---
+++ src/main/java/com/example/antaliya_taxi_service/contoler/HomePageController.java
@@ -0,0 +1,31 @@
+package com.example.antaliya_taxi_service.controller;
+
+import com.example.antaliya_taxi_service.enums.Currency;
+import com.example.antaliya_taxi_service.service.RouteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+@RequiredArgsConstructor
+public class HomePageController {
+
+    private final RouteService routeService;
+
+    @GetMapping
+    public String homePage(Model model) {
+        // Получаем список всех доступных мест отправления
+        List<String> pickupLocations = routeService.getAllActivePickupLocations();
+
+        // Добавляем данные в модель
+        model.addAttribute("pickupLocations", pickupLocations);
+        model.addAttribute("defaultCurrency", Currency.TRY);
+
+        return "homeV1";  // Возвращаем имя вашего шаблона
+    }
+}
